0a611cbcf6d8a0d4dd4b37666545b2a28336e2a4
viswasarathi <viswasarathi03@gmail.com>
2023-08-09 23:14:27 +0530

    updated 2

--- a2/views.py
+++ a2/views.py
@@ -6,5 +6,24 @@ from .models import Places
 def index(request):
     place1 = Places()
     place1.name = "chennai"
-    return render(request,"index.html",{"place1":place1})
+    place1.cap = "hot city"
+    place1.review = 4
+    place1.days = 4
+
+    
+    place2 = Places()
+    place2.name = "coimbatore"
+    place2.cap = "chill city - manchester of tamilnadu"
+    place2.review = 5
+    place2.days = 10
+
+    place3 = Places()
+    place3.name = "madurai"
+    place3.cap = "moderate city"
+    place3.review = 4
+    place3.days = 7
+
+    allPlace = [place1,place2,place3]
+
+    return render(request,"index.html",{"places":allPlace})
 
