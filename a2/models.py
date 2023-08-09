19493f6c412ee34ca7a0df548ab35542adf736b8
viswasarathi <viswasarathi03@gmail.com>
2023-08-09 22:43:43 +0530

    update1

--- a2/models.py
+++ a2/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Places(models.Model):
+   name:str
+   cap:str
+   review:int
+   days:int
--- a2/views.py
+++ a2/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Places
 
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html",{})
+    place1 = Places()
+    place1.name = "chennai"
+    return render(request,"index.html",{"place1":place1})
+
