32a1072586964fdc1f8129ea72cd5285900a3a59
viswasarathi <viswasarathi03@gmail.com>
2023-08-10 20:27:49 +0530

    create a superuser

--- a2/models.py
+++ a2/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Places(models.Model):
-   name:str
-   cap:str
-   review:int
-   days:int
-   offer:bool
+   name = models.CharField(max_length=200)
+   cap = models.TextField()
+   review = models.IntegerField()
+   days = models.IntegerField()
+   offer = models.BooleanField(default=False)
