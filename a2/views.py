affd28e3dc937a6328c24917a95eb44c126bc888
viswasarathi <viswasarathi03@gmail.com>
2023-08-10 20:51:53 +0530

    Add and fetch data from database

--- a2/views.py
+++ a2/views.py
@@ -4,26 +4,7 @@ from .models import Places
 # Create your views here.
 
 def index(request):
-    place1 = Places()
-    place1.name = "chennai"
-    place1.cap = "hot city"
-    place1.review = 4
-    place1.offer = False
-
-    
-    place2 = Places()
-    place2.name = "coimbatore"
-    place2.cap = "chill city - manchester of tamilnadu"
-    place2.review = 5
-    place2.offer = True
-
-    place3 = Places()
-    place3.name = "madurai"
-    place3.cap = "moderate city"
-    place3.review = 4
-    place3.offer = False
-
-    allPlace = [place1,place2,place3]
+    allPlace = Places.objects.all()
 
     return render(request,"index.html",{"places":allPlace})
 
